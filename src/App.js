bf3ce8a828c7a499a318b3e3bb4a052dda5af575
Manuel Arredondo <manuel.arredondo@nuance.com>
2021-05-26 19:14:46 -0400

    swapped image with my own, linked to my GitHub

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://github.com/zmanx.png"} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello React World
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/zmanx/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My GitHub Profile
         </a>
       </header>
     </div>
